d70cbe1ed4574d24a0965dd3dcaae88faed8a71d
Jeffrey Overmeer <jeffrey.overmeer@gmail.com>
2017-11-13 22:02:03 +0100

    fixex gmaps apikey

--- source/assets/javascripts/scripts.js
+++ source/assets/javascripts/scripts.js
@@ -21,21 +21,4 @@
 
   });
 
-
-
-  var formData = {
-  "first_name": $('#first_name').val(),
-  "last_name": $('#last-name').val(),
-  "email": $('#email').val()  
-}
-
-// Then listen to the form submit event
-$('#kennismaken').submit(function(evt) {
-  evt.preventDefault(); //Prevent the default form submit action
-
-  // You have formData here and can do this:
- 
-
-  firebase.database().ref().push( formData ); // Adds the new form data to the list under formDataTree node
-})
 });
